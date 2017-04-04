84e9d90d9169b74dc99518842d08c66e3c846c49
Adam Franzen <afran@PC437.cscc.unc.edu>
2017-04-04 13:08:06 -0400

    first method to handle maintaining reference to this in StorePicker

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
+/*
+  constructor extends
+
+*/
 class StorePicker extends React.Component {
+  // super runs React.Component first
+  constructor() {
+    super();
+    this.goToStore = this.goToStore.bind(this);
+  }
+  // and then we sprinkle on our own stuff
+  goToStore(event) {
+    event.preventDefault();
+    console.log('you changed the URL');
+    //first grab the text from box
+    console.log(this);
+    //transition from / to /store/:storeId
+  }
+
   render() {
     // Any where else
+    // Render is a methd bound to the component
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
         <button type="submit">Visit Store →</button>
       </form>
     )
