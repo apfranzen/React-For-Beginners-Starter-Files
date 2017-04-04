bf94a083127a064e68e914f848a97a09ec3a7413
Adam Franzen <afran@PC437.cscc.unc.edu>
2017-04-04 12:39:59 -0400

    video 99

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 import './css/style.css';
 import App from './components/App'
+
 // if this is just a string, it will try to look for a NPM package
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker}/>
+        <Match pattern="/store/:storeId" component={App}/>
+        <Miss component={NotFound}/>
+      </div>
+    </BrowserRouter>
+  )
+}
 
 //targer tp the react element, query selector
-render(<App/>, document.querySelector('#main'))
+render(<Root/>, document.querySelector('#main'))
