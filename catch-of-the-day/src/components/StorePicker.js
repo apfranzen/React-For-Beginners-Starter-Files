52abb9931c6a43ac89373ce102b658d475073372
Adam Franzen <afran@PC437.cscc.unc.edu>
2017-04-04 13:12:28 -0400

    this.goToStore.bind(this)

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -7,16 +7,16 @@ import { getFunName } from '../helpers';
 */
 class StorePicker extends React.Component {
   // super runs React.Component first
-  constructor() {
-    super();
-    this.goToStore = this.goToStore.bind(this);
-  }
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
   // and then we sprinkle on our own stuff
   goToStore(event) {
     event.preventDefault();
     console.log('you changed the URL');
     //first grab the text from box
-    console.log(this);
+    console.log(this.storeInput.value);
     //transition from / to /store/:storeId
   }
 
@@ -24,7 +24,7 @@ class StorePicker extends React.Component {
     // Any where else
     // Render is a methd bound to the component
     return (
-      <form className="store-selector" onSubmit={this.goToStore}>
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
         <h2>Please Enter A Store</h2>
         <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
         <button type="submit">Visit Store →</button>
