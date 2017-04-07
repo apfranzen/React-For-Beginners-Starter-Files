adbaae0427061009450a0efc7f025b4224a82458
Adam Franzen <afran@PC437.cscc.unc.edu>
2017-04-07 11:45:30 -0400

    completed video16

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -15,6 +15,7 @@ class App extends React.Component {
     // this will bind fish method to App component - you could do $r.addFish({name: 'cool', price: 1000})
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
 
     // getinitialstate
     this.state = {
@@ -45,6 +46,15 @@ class App extends React.Component {
     });
   }
 
+  addToOrder(key) {
+    // take a copy of our state
+    const order = {...this.state.order};
+    // update or add the new number of fish ordered
+    order[key] = order[key] + 1 || 1; // if there is already 1 ordered, ++1
+    // update our state
+    this.setState({ order });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -54,7 +64,7 @@ class App extends React.Component {
             {
               Object
                 .keys(this.state.fishes)
-                .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+                .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
             }
           </ul>
         </div>
